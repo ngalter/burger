1cf4e81aaaf187a66c9622b5f9c4e4a2ed8f9149
Nancy Alter <nancy.alter@gmail.com>
2020-02-23 10:00:03 -0500

    move work into repo
    move initial hw into repo

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Philly Burger');
+INSERT INTO burgers (burger_name) VALUES ('Burger Deluxe');
+INSERT INTO burgers (burger_name) VALUES ('Go Cubbies Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Green Burger',TRUE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('The Carnivore',TRUE);
