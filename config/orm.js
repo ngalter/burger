94c7ce7fb3d047fa4567994db3891d2f468f1752
Nancy Alter <nancy.alter@gmail.com>
2020-02-24 17:04:47 -0500

    completed controller name changes and clear commented code
    finished controller name change. cleaned commented code that I won't use

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,6 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-// function printQuestionMarks(num) {
-//   var arr = [];
-
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-
-//   return arr.toString();
-// }
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
@@ -51,7 +36,7 @@ var orm = {
     });
   },
   insertOne: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO ?? (??) VALUES (?);"; 
+    var queryString = "INSERT INTO ?? (??) VALUES (?);";  
     connection.query(queryString, [table, cols, vals,], function(err, result) {
       if (err) {
         throw err;
@@ -60,16 +45,6 @@ var orm = {
     });
   },
 
-//   updateOne: function(table, objColVals, condition, cb) {
-//     var queryString = "UPDATE ?? SET ? WHERE ?;";
-//     connection.query(queryString,[table, objColVals, condition], function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-//       cb(result);
-//     });
-//   }
-// };
 updateOne: function(table,objColVals,condition,cb){
   var queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition};`;
   connection.query(queryString,function(err,result){
@@ -80,26 +55,6 @@ updateOne: function(table,objColVals,condition,cb){
   });
 }
 };
-
-  //   // Set burger devoured status to true.
-  // updateOne: function (table, objColVals, condition, cb) {
-  //   var queryString = "UPDATE " + table;
-  //   queryString += " SET ";
-  //   queryString += objToSql(objColVals);
-  //   queryString += " WHERE ";
-  //   queryString += condition;
-
-  //   console.log(queryString);
-
-  //   connection.query(queryString, function (err, result) {
-  //     if (err) {
-  //       throw err
-  //     }
-  //     cb(result);
-  //   });
-  // }
-  // };
-
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
