6ffebfe04700f51979cd21646fce889dc27cfec4
Nancy Alter <nancy.alter@gmail.com>
2020-02-24 16:48:34 -0500

    change file name to match assignment req.
    change controller file name to burger_controller.js as per directions

--- config/orm.js
+++ config/orm.js
@@ -6,15 +6,15 @@ var connection = require("../config/connection.js");
 // In order to write the query, we need 3 question marks.
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
-  var arr = [];
+// function printQuestionMarks(num) {
+//   var arr = [];
 
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
+//   for (var i = 0; i < num; i++) {
+//     arr.push("?");
+//   }
 
-  return arr.toString();
-}
+//   return arr.toString();
+// }
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
